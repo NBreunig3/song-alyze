edf2c4094b14c977201ca5885d7475fc19912dfa
Nathan Breunig <nathanbreunig3@gmail.com>
2020-06-15 20:54:20 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,12 +1,12 @@
 # Config file to hold configuration info
 # App Spotify authorization id's
 spotify_ids = {
-    "client_id" : "2f1c5eb351e8437da2a5b7b9d4b38df7",
-    "client_secret" : "23c1b0cacbc5457d87a6f20a627fd13c"
+    "client_id" : "",
+    "client_secret" : ""
 }
 
 # Config for genius API id's
 genius_ids = {
-    "client_id" : "3vhQodmnehRSsj03yMbjxcVPJ5xn89xDAkaEkDHd9Z64bfG-974Uzv7g1f0t8xM4",
-    "client_secret" : "uou6q9CuLLiieSe8J31HTMHpbWaxBDD1E8ceeItf1ftgFCJKi4FgLWRZNlK-R3OyfUi7jKz4gzH7LlP4Hn1WYQ"
+    "client_id" : "",
+    "client_secret" : ""
 }
